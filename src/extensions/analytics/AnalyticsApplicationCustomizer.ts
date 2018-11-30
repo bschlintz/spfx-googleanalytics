31119f57c3dc75fb079c00c3a8451ec12d80d430
Brad Schlintz <brschlin@microsoft.com>
2018-11-30 12:10:15 -0600

    Core functionality working

--- src/extensions/analytics/AnalyticsApplicationCustomizer.ts
+++ src/extensions/analytics/AnalyticsApplicationCustomizer.ts
@@ -1,48 +1,107 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { AnalyticsSettingsButton, IAnalyticsSettingsButtonProps } from './components/AnalyticsSettingsButton';
 import { override } from '@microsoft/decorators';
-import { Log } from '@microsoft/sp-core-library';
 import {
-  BaseApplicationCustomizer
+  BaseApplicationCustomizer, PlaceholderContent, PlaceholderName
 } from '@microsoft/sp-application-base';
-import { Dialog } from '@microsoft/sp-dialog';
 
-import * as strings from 'AnalyticsApplicationCustomizerStrings';
-
-import AnalyticsConfig from './AnalyticsConfig';
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
+import { SPPermission } from '@microsoft/sp-page-context';
 
 const LOG_SOURCE: string = 'AnalyticsApplicationCustomizer';
 
-export interface IAnalyticsApplicationCustomizerProperties { }
+export interface IAnalyticsApplicationCustomizerProperties {
+  googleTrackingId: string;
+}
 
 export default class AnalyticsApplicationCustomizer
   extends BaseApplicationCustomizer<IAnalyticsApplicationCustomizerProperties> {
 
+  private _bottomPlaceholder: PlaceholderContent | undefined;
+
   @override
   public async onInit(): Promise<void> {
-    let analyticsConfig = new AnalyticsConfig();
-    await analyticsConfig.setup(this.context);
-    let googleTrackingId: string = await analyticsConfig.getGoogleTrackingId();
+    let googleTrackingId: string = this.properties.googleTrackingId;
 
-    if (!googleTrackingId) {
-      console.warn(LOG_SOURCE, "Google Analytics Tracking ID: Not Specified");
-      googleTrackingId = await analyticsConfig.setGoogleTrackingId("UA-129865562-1");
+    //Wire up settings button if the user is a site owner
+    if (this.checkIfCurrentUserIsSiteOwner()) {
+      this.context.placeholderProvider.changedEvent.add(this, this.renderPlaceHolders);
+    }
 
-    } else {
+    //If we have a google tracking id, setup ga on page, otherwise log warning
+    if (googleTrackingId) {
       console.log(LOG_SOURCE, `Google Analytics Tracking ID: ${googleTrackingId}`);
+      this.setupGoogleAnalytics(googleTrackingId);
+    } else {
+      console.warn(LOG_SOURCE, "Google Analytics Tracking ID: Not Specified");
+    }
+  }
 
-      //Add Google Analytics Script Tag to Page
-      var gtagScript = document.createElement("script");
-      gtagScript.type = "text/javascript";
-      gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${googleTrackingId}`;
-      gtagScript.async = true;
-      document.head.appendChild(gtagScript);
+  private renderPlaceHolders(): void {
+    if (!this._bottomPlaceholder) {
+      this._bottomPlaceholder = this.context.placeholderProvider.tryCreateContent(PlaceholderName.Bottom);
 
-      //Invoke Google Analytics Page Tracker
-      window["dataLayer"] = window["dataLayer"] || [];
-      window["gtag"] = window["gtag"] || function gtag() {
-        window["dataLayer"].push(arguments);
+      if (this._bottomPlaceholder.domElement) {
+        const analyticsSettingsButton: React.ReactElement<IAnalyticsSettingsButtonProps> = React.createElement(AnalyticsSettingsButton, {
+          googleTrackingId: this.properties.googleTrackingId,
+          onSaveFunction: this.updateGoogleTrackingId.bind(this)
+        } as IAnalyticsSettingsButtonProps);
+        ReactDOM.render(analyticsSettingsButton, this._bottomPlaceholder.domElement);
       }
-      window["gtag"]('js', new Date());
-      window["gtag"]('config', googleTrackingId);
+    }
+  }
+
+  private checkIfCurrentUserIsSiteOwner(): boolean {
+    return this.context.pageContext.web.permissions.hasPermission(SPPermission.manageWeb);
+  }
+
+  private setupGoogleAnalytics(googleTrackingId: string): void {
+    //Add Google Analytics Script Tag to Page
+    var gtagScript = document.createElement("script");
+    gtagScript.type = "text/javascript";
+    gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${googleTrackingId}`;
+    gtagScript.async = true;
+    document.head.appendChild(gtagScript);
+
+    //Invoke Google Analytics Page Tracker
+    window["dataLayer"] = window["dataLayer"] || [];
+    window["gtag"] = window["gtag"] || function gtag() {
+      window["dataLayer"].push(arguments);
+    };
+    window["gtag"]('js', new Date());
+    window["gtag"]('config', googleTrackingId);
+  }
+
+  private async updateGoogleTrackingId(googleTrackingId: string): Promise<void> {
+    let customAction = await this.getCustomActionByComponentId(this.manifest.id);
+
+    try {
+      let postCustomActionUri = `${this.context.pageContext.web.absoluteUrl}/_api/web/usercustomactions('${customAction.Id}')`;
+      await this.context.spHttpClient.post(postCustomActionUri, SPHttpClient.configurations.v1, {
+        headers: {
+          "X-HTTP-Method": "MERGE",
+          "content-type": "application/json; odata=nometadata"
+        },
+        body: JSON.stringify({
+          "ClientSideComponentProperties": `{ "googleTrackingId": "${googleTrackingId}" }`
+        })
+      });
+    } catch (error) {
+      console.log(`ERROR: Unable to update custom action with id ${customAction.id}`, error);
+    }
+  }
+
+  private async getCustomActionByComponentId(componentId: string): Promise<any> {
+    try {
+      let getCustomActionUri = `${this.context.pageContext.web.absoluteUrl}/_api/web/usercustomactions`;
+      getCustomActionUri += `?$filter=ClientSideComponentId eq guid'${componentId}'`;
+      let getCustomActionResponse: SPHttpClientResponse = await this.context.spHttpClient.get(getCustomActionUri, SPHttpClient.configurations.v1);
+      let getCustomActionResult = await getCustomActionResponse.json();
+      return getCustomActionResult.value && getCustomActionResult.value.length > 0 ? getCustomActionResult.value[0] : null;
+    }
+    catch (error) {
+      console.log(`ERROR: Unable to fetch custom action with ClientSideComponentId ${componentId}`, error);
     }
   }
 }
---
+++ src/extensions/analytics/components/AnalyticsSettingsButton.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import styles from './AnalyticsSettingsButton.module.scss';
+import { PrimaryButton, Button, Dialog, DialogFooter, TextField, IconButton } from '@microsoft/office-ui-fabric-react-bundle';
+
+export class IAnalyticsSettingsButtonProps {
+  public googleTrackingId: string;
+  public onSaveFunction: Function;
+}
+
+export class AnalyticsSettingsButton extends React.Component<IAnalyticsSettingsButtonProps, { showModal: boolean; }> {
+  private _textGoogleAnalyticsTrackingId: TextField;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+  public render(): React.ReactElement<any> {
+    return (
+      <div className={styles.container}>
+        <IconButton
+          iconProps={{ iconName: "BarChart4", className: styles.iconButton }}
+          onClick={this._showModal}
+          title="Google Analytics Settings"
+          ariaLabel="Google Analytics Settings"
+        />
+        <Dialog
+          isOpen={this.state.showModal}
+          onDismiss={this._closeModal}
+          isBlocking={false}
+          title={"Google Analytics Settings"}
+          subText={"Update Google Analytics Tracking ID used by this site."}
+        >
+          <TextField
+            label="Tracking ID"
+            defaultValue={this.props.googleTrackingId}
+            ref={r => this._textGoogleAnalyticsTrackingId = r}
+          ></TextField>
+          <DialogFooter>
+            <PrimaryButton onClick={this._onClickSave} text="Save" />
+            <Button onClick={this._closeModal} text="Cancel" />
+          </DialogFooter>
+        </Dialog>
+      </div>
+    );
+  }
+
+  private _onClickSave = async (): Promise<void> => {
+    try {
+      await this.props.onSaveFunction(this._textGoogleAnalyticsTrackingId.value);
+      //refresh page on save
+      location.reload();
+
+      // this.setState({ showModal: false });
+    }
+    catch (error) {
+      console.log("ERROR: Unable to save Google Analytics Tracking Id", error);
+      this.setState({ showModal: false });
+    }
+  }
+
+  private _showModal = (): void => {
+    this.setState({ showModal: true });
+  }
+
+  private _closeModal = (): void => {
+    this.setState({ showModal: false });
+  }
+}
